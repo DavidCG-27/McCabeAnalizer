7241c1f467e8d1012427019d38b065d292cbd8a3
David Covián Gómez <dcoviangomez@gmail.com>
2025-02-06 00:44:33 +0100

    Update McCabeUI.java

--- src/view/McCabeUI.java
+++ src/view/McCabeUI.java
@@ -1,5 +1,15 @@
 package view;
 
-public class McCabeUI {
-
+/**
+ * Clase que representa la vista y se encarga de mostrar los resultados al usuario.
+ */
+public class ComplexityView {
+    /**
+     * Muestra la complejidad ciclomatica calculada.
+     * 
+     * @param complejidad Valor de la complejidad ciclomatica.
+     */
+    public void mostrarResultado(int complejidad) {
+        System.out.println("La complejidad ciclomatica del código es: " + complejidad);
+    }
 }
