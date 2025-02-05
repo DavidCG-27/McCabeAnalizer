ff7fce1ec6b99336f8ae9168929d122d4f64c8ee
David Covián Gómez <dcoviangomez@gmail.com>
2025-02-06 00:45:23 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+package main;
+
+import model.ComplexityModel;
+import view.ComplexityView;
+import controller.ComplexityController;
+
+/**
+ * Clase principal que inicia el programa y gestiona la ejecución.
+ */
 public class Main {
-    
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso: java Main <ruta_archivo.java>");
+            return;
+        }
+
+        McCabeModel modelo = new McCabeModel();
+        McCabeUI vista = new McCabeUI();
+        McCabeController controlador = new McCabeController(modelo, vista);
+
+        controlador.analizarCodigo(args[0]);
+    }
 }
